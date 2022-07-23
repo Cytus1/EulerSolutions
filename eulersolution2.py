bd401e95fd4a9c8ec5dd9c2a82afe41b076fc956
Cytus1 <109835252+Cytus1@users.noreply.github.com>
2022-07-23 11:36:38 -0400

    projecteuler.net Question 2 Solution
    Python solution for question 2, written with clear codes.

---
+++ eulersolution2.py
@@ -0,0 +1,15 @@
+def fib(x):  # x = number of term
+    a = 0  # starting point
+    b = 1
+    addition = 0 # sum of a and b
+    even_list = []
+    while b < x:
+        if addition % 2 == 0:
+            even_list.append(addition)
+            print(sum(even_list))
+
+        addition = a + b
+        b = a
+        a = addition
+
+fib(4000000)
