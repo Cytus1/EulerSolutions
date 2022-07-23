487bcf6d44087dd397fe36588d72ee4af04deea2
Cytus1 <109835252+Cytus1@users.noreply.github.com>
2022-07-23 00:36:47 -0400

    Update euler_problem_number1.py

--- euler_problem_number1.py
+++ euler_problem_number1.py
@@ -1,5 +1,3 @@
-#function call for 3 and 5
-
 sum=0
 for number in range(3,1000):
     if number % 3 == 0:
@@ -7,9 +5,3 @@ for number in range(3,1000):
     elif number % 5 == 0:
         sum += number
 print(sum) #233168
-
-#3 to 1000
-
-#sum
-
-#multiple of 3 and 5
