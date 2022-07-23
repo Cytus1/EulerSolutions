3b9f11fb6d91825aa867b1dd1439335aef319e67
Cytus1 <109835252+Cytus1@users.noreply.github.com>
2022-07-22 22:28:19 -0400

    projecteuler.net solution ; question 1
    This is a for-loop solution in python.

---
+++ euler_problem_number1.py
@@ -0,0 +1,15 @@
+#function call for 3 and 5
+
+sum=0
+for number in range(3,1000):
+    if number % 3 == 0:
+        sum += number
+    elif number % 5 == 0:
+        sum += number
+print(sum) #233168
+
+#3 to 1000
+
+#sum
+
+#multiple of 3 and 5
