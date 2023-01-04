65dcd70ba13c04e71d4a4ab1ec8cfffc9a8bd271
Weiyang Zhang <109835252+Cytus1@users.noreply.github.com>
2023-01-04 15:19:55 -0500

    Create projecteuler.net problem 16 solution.py

---
+++ projecteuler.net problem 16 solution.py
@@ -0,0 +1,21 @@
+string = str(2**1000)
+space_seperated_str_int = ""
+
+for single_number in range(len(string)):
+    space_seperated_str_int += string[single_number]
+    space_seperated_str_int += " "
+
+str_number_array = space_seperated_str_int.split(" ")
+str_number_array.pop(-1)
+
+int_number_array = []
+
+for i in str_number_array:
+    int_number_array.append(int(i))
+
+sum = 0
+
+for i in int_number_array:
+    sum += i
+
+print(sum)  #   1366
