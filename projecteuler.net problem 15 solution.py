c94aac8ff91badfd4590f7ba2cf85f6bf0c62408
Weiyang Zhang <109835252+Cytus1@users.noreply.github.com>
2023-01-04 14:32:34 -0500

    Create projecteuler.net problem 15 solution.py

---
+++ projecteuler.net problem 15 solution.py
@@ -0,0 +1,10 @@
+import math
+
+
+print(int(math.factorial(40) / math.factorial(20)**2))
+
+"""
+The answer was 137846528820, turns out there was a simple formula for this question.
+I suppose the main takeaway on this problem is to not brute force it, and try convert
+it into a math problem.
+"""
