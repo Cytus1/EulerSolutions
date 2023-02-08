aa7b56923cf0748af88133c87508b26d279e204f
Weiyang Zhang <109835252+Cytus1@users.noreply.github.com>
2023-02-08 16:44:55 -0500

    Create projecteuler.net question 28 solution.py

---
+++ projecteuler.net question 28 solution.py
@@ -0,0 +1,25 @@
+"""
+Main point: Iterate
+
+Append only odd, create gap for where gap =  n += 1 each 4 number.
+"""
+
+grid, scan_dia = [x for x in range(1, (1001 * 1001) + 1) if x % 2 != 0], [1]
+total_spiral_num = 1001 * 1001
+spiral_gap_till_edge, four_counter, base_rate = 0, 0, 1
+
+while scan_dia[-1] != grid[-1]:
+    
+    if four_counter == 4:
+        four_counter = 0
+        base_rate += 1
+    
+    spiral_gap_till_edge += base_rate
+    
+    scan_dia.append(grid[spiral_gap_till_edge])
+    four_counter += 1
+
+sum = 0
+for dia_num in scan_dia:
+    sum += dia_num
+print(sum)  #   669171001
